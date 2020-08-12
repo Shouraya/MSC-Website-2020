ecc46b4c296d3ab1dcc71804a8ee5c132db40123
Shouraya <shourayagoyal2406@gmail.com>
2020-08-12 18:44:27 +0530

    sponsor carousel done, images to be fixed

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -37,3 +37,27 @@ function test(){
   $(".navbar-toggler").click(function(){
     setTimeout(function(){ test(); });
   });
+
+  // Carousel Sponsor
+  $(document).ready(function(){
+    $('.logos').slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      arrows: false,
+      dots: false,
+      pauseOnHover: true,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4
+        }
+      }, {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3
+        }
+      }]
+    });
+  });
